62c58029d802dbfafeac486351bb3cbd2924ae36
Sandeep D S <sandeepds7273@gmail.com>
2024-06-05 19:55:55 +0530

    added total entries variable
    to keep track of the number of people entered at one go

--- index.js
+++ index.js
@@ -2,9 +2,12 @@
 
 let saveEl = document.getElementById("save-el")
 let countEl = document.getElementById("count-el")
+let totalEl = document.getElementById("total-el")
+let copy = "Total entries: "
 console.log(countEl)
 
 let count = 0
+let total = 0
 
 console.log(count)
 function increment() {
@@ -16,6 +19,8 @@ function increment() {
 function save(){
   console.log(count)
   saveEl.textContent += (count + " - " )
+  total += count
+  totalEl.textContent = copy + total
   count = 0
   countEl.textContent = 0 
 }
